4ac36caa5bf9212ce4b84f2ad60ee9c46974ac1c
Tri Tran <tri1684@gmail.com>
2020-02-21 22:03:50 +0700

    update migrate

--- database/migrations/2020_02_20_122542_create_notifications_table.php
+++ database/migrations/2020_02_20_122542_create_notifications_table.php
@@ -15,10 +15,11 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->smallInteger('type')->comment('1: advice, 2: news, 3: video, 4: book, 5: other');
+
             $table->string('title', 255);
             $table->string('body', 1000);
-            $table->bigInteger('item_id')->nullable();
+            $table->smallInteger('notification_type')->nullable();
+            $table->bigInteger('notification_id')->nullable();
             $table->smallInteger('send_to')->default(0)->comment('0: all; 1 : iOS, 2: Android');
             $table->timestamps();
         });
