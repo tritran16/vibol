53080f7939a337d7ceef135b687d9241d4884496
Tri Tran <tri1684@gmail.com>
2020-02-26 00:48:45 +0700

    fix bug

--- app/Http/Controllers/API/v1/BooksController.php
+++ app/Http/Controllers/API/v1/BooksController.php
@@ -22,7 +22,9 @@ class BooksController extends ApiController
      */
     public function index(Request $request){
         $keyword = $request->get('keyword');
-        $books = Book::select('books.*', 'like_books.id  AS  like_book_id')
+        $books = Book::select('books.id', 'name', 'books.thumbnail', 'books.category_id' , 'books.filename', 'books.link',
+            'books.page_number', 'books.author','books.description', 'books.status', 'books.likes', 'books.views', 'books.is_hot',
+            'books.created_at', 'books.updated_at', 'like_books.id  AS  like_book_id')
             ->with('category')
             ->leftJoin('like_books', 'like_books.book_id', '=', 'books.id')
             ->where('status', 1);
@@ -46,7 +48,9 @@ class BooksController extends ApiController
             default:
                 // TODO
         }
-
+        $books->groupBy('books.id', 'name', 'books.thumbnail', 'books.category_id' , 'books.filename', 'books.link',
+            'books.page_number', 'books.author','books.description', 'books.status', 'books.likes', 'books.views', 'books.is_hot',
+            'books.created_at', 'books.updated_at', 'like_books.id');
         // short by id desc
         $books->orderBy('created_at', 'DESC');
         $querystringArray = $request->only(['keyword','order','hot']);
