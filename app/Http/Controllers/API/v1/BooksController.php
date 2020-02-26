b97e019feb854f326af1a2fdfc1097105f8e1d6b
Tri Tran <tri1684@gmail.com>
2020-02-26 07:12:46 +0700

    update - test

--- app/Http/Controllers/API/v1/BooksController.php
+++ app/Http/Controllers/API/v1/BooksController.php
@@ -53,7 +53,7 @@ class BooksController extends ApiController
             'books.created_at', 'books.updated_at', 'like_books.id');
         // short by id desc
         $books->orderBy('created_at', 'DESC');
-        
+
         $data = $books->paginate(API_PAGE_LIMITED);
         return $this->successResponse(new BookCollection($data));
     }
