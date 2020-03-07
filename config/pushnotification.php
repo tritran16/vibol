0a7b52a89bdcbcbcc7c7a4ff01aeaae2b2fafdfe
Tri Tran <tri1684@gmail.com>
2020-03-07 22:16:34 +0700

    update- upload file name with url encode

--- config/pushnotification.php
+++ config/pushnotification.php
@@ -15,7 +15,7 @@ return [
         'apiKey' => 'My_ApiKey',
     ],
     'apn' => [
-        'certificate' => storage_path("app/iosCertificates/ios_dev.pem"),
+        'certificate' => env('APN_ENV') != 'production'? storage_path().'/app/iosCertificates/ios_dev.pem' : storage_path().'/app/iosCertificates/ios_production.pem' ,//storage_path("app/iosCertificates/ios_dev.pem"),
         'passPhrase' => '1', //Optional
         //'passFile' => __DIR__ . '/iosCertificates/yourKey.pem', //Optional
         'dry_run' => false,
