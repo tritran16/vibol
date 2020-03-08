91ea5b57dcd8230708638f7816fcb1cb9ff655ad
Tri Tran <tri1684@gmail.com>
2020-03-08 23:11:24 +0700

    push notify

--- config/push-notification.php
+++ config/push-notification.php
@@ -4,7 +4,7 @@ return array(
 
     'appNameIOS'     => array(
         'environment' =>'production',
-        'certificate' => storage_path().'/app/iosCertificates/ios_production.pem' ,
+        'certificate' =>env('APN_ENV') != 'production'? storage_path().'/app/iosCertificates/ios_dev.pem' : storage_path().'/app/iosCertificates/ios_production.pem' ,
         'passPhrase'  =>'1',
         'service'     =>'apns'
     ),
