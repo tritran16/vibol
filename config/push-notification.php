f8a4350223f1723c45b60a36feeeb0019f7f9f01
Tri Tran <tri1684@gmail.com>
2020-03-08 23:16:56 +0700

    test

--- config/push-notification.php
+++ config/push-notification.php
@@ -3,7 +3,7 @@
 return array(
 
     'appNameIOS'     => array(
-        'environment' =>'production',
+        'environment' =>'development',
         'certificate' =>env('APN_ENV') != 'production'? storage_path().'/app/iosCertificates/ios_dev.pem' : storage_path().'/app/iosCertificates/ios_production.pem' ,
         'passPhrase'  =>'1',
         'service'     =>'apns'
