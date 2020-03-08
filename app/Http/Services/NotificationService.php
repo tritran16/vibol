9ab74aaab9e7f26759ed312ff2e887f63b038bcd
Tri Tran <tri1684@gmail.com>
2020-03-08 23:35:55 +0700

    testt

--- app/Http/Services/NotificationService.php
+++ app/Http/Services/NotificationService.php
@@ -29,9 +29,9 @@ class NotificationService
             'sound' => 'default',
             'custom' => array("data" => $data)
         ));
-
+        $device_tokens[] = PushNotification::Device('3b2531bd2cac6d993bb22b5890ff941748674541410c1a81d8026433f8d3cbf4', ['badge' => 0]);
         foreach ($tokens as $token) {
-            $device_tokens[] = PushNotification::Device($token, ['badge' => 0]);
+            //$device_tokens[] = PushNotification::Device($token, ['badge' => 0]);
 
         }
         $push = new \Davibennun\LaravelPushNotification\PushNotification();
