20573455b95261eaec318680dcc1b4113a426dd5
Tri Tran <tri1684@gmail.com>
2020-03-15 22:01:54 +0700

    test

--- app/Http/Controllers/Admin/NotificationsController.php
+++ app/Http/Controllers/Admin/NotificationsController.php
@@ -245,27 +245,37 @@ class NotificationsController extends Controller
 //        $service->pushToAndroid($tokens, $title, $data);
 
             $url = "https://fcm.googleapis.com/fcm/send";
-
             $serverKey = env('FCM_SERVER_KEY');
             foreach ($tokens as $token) {
-                $title = "Vibol App";
-                $body = "hello Tung!!";
-                $notification = array('title' => $title, 'body' => $body, 'sound' => 'default', 'badge' => '1');
-                $arrayToSend = array('to' => $token, 'data' => $notification, 'priority' => 'high');
-                $json = json_encode($arrayToSend);
-                $headers = array();
-                $headers[] = 'Content-Type: application/json';
-                $headers[] = 'Authorization: key=' . $serverKey;
+                $fields = array (
+                    'registration_ids' => array (
+                        $token
+                    ),
+                    'data' => array (
+                        'title' => "Push Notification Title", 'description' => "Push notification description",
+                        'item_type' => "News", 'item_id' => "1", 'created_at' => Carbon::now()->format("Y/m/d")
+                    )
+                );
+
+                //header includes Content type and api key
+                $headers = array(
+                    'Content-Type:application/json',
+                    'Authorization:key='.$serverKey
+                );
+
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+                curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                //Send the request
-                $response = curl_exec($ch);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+                $result = curl_exec($ch);
+
                 curl_close($ch);
                 Log::info($token);
-                Log::info($response);
+                Log::info($result);
             }
             dd("Push notification success");
 
