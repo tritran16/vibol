e4456c2aa664934b2a859db8cbed79fbb829c483
Tri Tran <tri1684@gmail.com>
2020-06-18 14:15:32 +0700

    update

--- app/Http/Controllers/API/v1/AboutController.php
+++ app/Http/Controllers/API/v1/AboutController.php
@@ -10,6 +10,9 @@ class AboutController extends ApiController
 {
     public function index(Request $request) {
        $about = About::first();
+       if (!$about) {
+           return $this->failedResponse(['not_exist' => "About Info not exist. Please login by admin and create it."]);
+       }
         $about->image = url($about->image) ;
         return $this->successResponse($about);
     }
