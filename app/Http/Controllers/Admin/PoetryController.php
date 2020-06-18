d1dc01d1b3945710fa9456bc6005830cef29e69c
Tri Tran <tri1684@gmail.com>
2020-06-18 12:17:49 +0700

    update

--- app/Http/Controllers/Admin/PoetryController.php
+++ app/Http/Controllers/Admin/PoetryController.php
@@ -60,8 +60,8 @@ class PoetryController extends Controller
         if ($request->file('thumbnail')){
             $thumbnail = $request->file('thumbnail');
             $thumbnail_name  = urlencode($thumbnail->getClientOriginalName()) ;
-            Storage::disk('public')->put('poetry/thumbnails/'. $poetry->id . '/'. $thumbnail_name, File::get($thumbnail));
-            Poetry::where('id', $poetry->id)->update([ 'thumbnail' => 'storage/poetry/thumbnails/'. $poetry->id . '/'.$thumbnail_name]);
+            Storage::disk('public')->put('poetry/images/'. $poetry->id . '/'. $thumbnail_name, File::get($thumbnail));
+            Poetry::where('id', $poetry->id)->update([ 'thumbnail' => 'storage/poetry/imagees/'. $poetry->id . '/'.$thumbnail_name]);
         }
         return redirect(route('poetrys.index'))->with('success', 'Created Poetry successfully!');
     }
--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -2,6 +2,7 @@
 return [
     'create.header' => 'Create New About Me',
     'edit.header' => 'Edit About Me',
+    'content' => 'Content',
     'image' => 'Image',
     'video_link' => 'Video Link',
 
