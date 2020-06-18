55c0fdee6c83e31cc9efe1fc7a59fdd63b254fce
Tri Tran <tri1684@gmail.com>
2020-06-18 12:15:18 +0700

    update

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'create.header' => 'Create New Poetry',
-    'edit.header' => 'Edit New Poetry',
+    'create.header' => 'Create New About Me',
+    'edit.header' => 'Edit About Me',
     'image' => 'Image',
     'video_link' => 'Video Link',
 
